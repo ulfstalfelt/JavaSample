254ef766b878f14087929a0e7f71977b6bed4e1a
Ulf <Ulf@Ulf-Dator>
2021-01-13 19:59:38 +0100

    ttt

---
+++ JavaSample/src/java/se/stalfelt/JavaSample/test1.java
@@ -0,0 +1,22 @@
+package se.stalfelt.JavaSample;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class test1 {
+
+	@Test
+	public void ctest() {
+		System.out.println("ctest");
+		HelloUlf.PrinHelloStalfelt();
+		HelloUlf.PrinHelloUlf();
+	}
+	@Test
+	public void atest() {
+		System.out.println("testadd");
+		System.out.println(AddInt.myMethod(5,6 ));
+	}
+
+
+}
