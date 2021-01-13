377a564d73ed060f66743ed993deee4a78977bd3
S35240 <S35240@B5495932.corp1.ad1.seb.net>
2021-01-13 13:10:09 +0100

    First src code

---
+++ JavaSample/src/java/se/stalfelt/JavaSample/HelloWorld.java
@@ -0,0 +1,23 @@
+/*
+ * HelloWorld.java
+ *
+ * created at Jan 13, 2021 by S35240 <YOURMAILADDRESS>
+ * 
+ * $Id: $
+ *
+ * Copyright (c) 2021 SEEBURGER AG, Germany. All Rights Reserved.
+ */
+package se.stalfelt.JavaSample;
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("HelloWorld");
+	}
+
+}
+
+
+
